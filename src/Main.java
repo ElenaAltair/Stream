6788c2b8f34ac5a441b57bea0d49531f0b1db7bb
Elena Petrova <eop79@mail.ru>
2024-01-29 12:09:44 +0300

    changed main, streammain

--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,24 @@ public class Main {
 
         List<Integer> intList = new ArrayList<>(Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4));
         List<Integer> intList2 = new ArrayList<>();
-        List<Integer> intList3 = new ArrayList<>();
 
         //Отфильтруйте положительные числа
         for (int number : intList) {
-            if (number > 0) {
+            if (number > 0 && number % 2 == 0) {
                 intList2.add(number);
             }
         }
 
-        //Найдите среди этих положительных чисел четные
-        for (int number : intList2) {
-            if (number % 2 == 0) {
-                intList3.add(number);
-            }
-        }
 
         //Отсортируйте отфильтрованные числа в порядке возрастания
-        Collections.sort(intList3);
+        Collections.sort(intList2);
 
         //Выведите результат на экран
-        for (int number : intList3) {
+        for (int number : intList2) {
             System.out.print(number + ", ");
         }
         System.out.println();
 
-        StreamMain stream1 = new StreamMain();
-        stream1.filterSort(intList);
+
     }
 }
--- src/StreamMain.java
+++ src/StreamMain.java
@@ -3,18 +3,17 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamMain {
-
-    public void filterSort(List intList) {
-
+    public static void main(String[] args) {
+        List<Integer> intList = new ArrayList<>(Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4));
         //получения потока из массива целых чисел
         //Stream<Integer> numbers = Stream.of(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         //numbers.filter(x -> x > 0).filter(x -> x % 2 == 0).sorted(Comparator.naturalOrder()).forEach(n -> System.out.println(n));
-
-
         Stream<Integer> stream = intList.stream();
         stream = stream.filter(x -> x > 0);
-        stream.filter(x -> x % 2 == 0).sorted(Comparator.naturalOrder()).forEach(System.out::println);
-
-
+        stream
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
     }
+
 }
